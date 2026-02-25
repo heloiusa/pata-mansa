dc0b7ffa1a74b5e71c5a1f6ff58fa74e9578ae66
heloiusa <heloisacosta2005@gmail.com>
2026-02-25 11:42:42 -0300

    modal novo banho adicionado

--- src/app/components/criar-banho/criar-banho.ts
+++ src/app/components/criar-banho/criar-banho.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, output, Output } from '@angular/core';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-criar-banho',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './criar-banho.html',
   styleUrl: './criar-banho.css',
 })
 export class CriarBanho {
+  iconAdd = faPlus;
+  iconClose = faTimes;
+  
+  /* @Output() aoFechar: Você está criando um "canal de saída" (porta de saída). O nome aoFechar é o evento que o componente Pai vai ficar "escutando".*/
+  @Output() aoFechar = new EventEmitter<void>();
 
+  /* new EventEmitter<void>(): Você está inicializando esse canal. O <void> significa que você só vai dar um "aviso", sem enviar nenhum dado junto (como se fosse apenas um sinal de "OK!").*/
+
+  fechar() {
+    this.aoFechar.emit();
+    /*this.aoFechar.emit(): Aqui é o momento em que o Filho aperta o botão do walkie-talkie e fala: "Atenção Pai, eu fui fechado!".*/
+  }
 }
--- src/app/components/tipos-banho/tipos-banho.ts
+++ src/app/components/tipos-banho/tipos-banho.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 // ícones sendo importados
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { CriarBanho } from '../criar-banho/criar-banho';
 
 @Component({
   selector: 'app-tipos-banho',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CriarBanho],
   templateUrl: './tipos-banho.html',
   styleUrl: './tipos-banho.css',
 })
@@ -14,4 +15,10 @@ export class TiposBanho {
   //Ícones
   iconSearch = faMagnifyingGlass;
   iconAdd = faPlus
+
+  exibirModal: boolean = false
+
+  abrirModal() {
+    this.exibirModal = true;
+  }
 }
